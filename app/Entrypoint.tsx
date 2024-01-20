c68874490290c6542c39c2370c31f5064f6d319e
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-01-20 22:58:23 +0530

    Entrypoint added

---
+++ app/Entrypoint.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import LandingScreen from './screens/LandingScreen';
+
+const Entrypoint = () => {
+    return (
+        <LandingScreen />
+    );
+};
+
+export default Entrypoint;
