780a67611791c19bea5e19184a1cbcc881190523
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-01-21 21:02:13 +0530

    PinCodeDialPad implementation done with some future improving comments

--- app/components/PinCodeDialPad/index.tsx
+++ app/components/PinCodeDialPad/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Dimensions,
   FlatList,
@@ -8,6 +8,13 @@ import {
   View,
 } from 'react-native';
 const { width } = Dimensions.get('window');
+
+// pin
+const pinLength = 4;
+const pinContainerSize = width / 2;
+const pinFullSize = pinContainerSize / pinLength;
+const pinSpacing = 10;
+const pinSize = pinFullSize - pinSpacing * 2;
 
 const dialPadSize = width * 0.2;
 const dialPadSizeFontSize = dialPadSize * 0.4;
@@ -26,12 +33,13 @@ const DialPad = ({
       keyExtractor={(_, index) => index?.toString()}
       numColumns={3}
       style={{ flexGrow: 0 }}
-      columnWrapperStyle={{ gap: _gap }}
+      columnWrapperStyle={{ gap: _gap * 2 }}
       contentContainerStyle={{ gap: _gap }}
       scrollEnabled={false}
       renderItem={({ item }) => {
         return (
           <TouchableOpacity
+            disabled={item === ''} // to remove pressablity of '' in the dialPad array even though it 's not visible
             onPress={() => {
               onPress(item);
             }}>
@@ -60,6 +68,7 @@ const DialPad = ({
 };
 
 export const PinCodeDialPad = () => {
+  const [code, setCode] = useState<number[]>([]);
   return (
     <View
       style={{
@@ -67,9 +76,41 @@ export const PinCodeDialPad = () => {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <DialPad onPress={() => {}} />
+      <View
+        style={{
+          flexDirection: 'row',
+          gap: pinSpacing * 2,
+          height: pinSize * 2,
+          marginBottom: pinSize,
+          alignItems: 'flex-end',
+        }}>
+        {[...Array(pinLength).keys()]?.map(i => {
+          const isSelected = !!code[i];
+          return (
+            <View
+              key={i}
+              style={{
+                width: pinSize,
+                borderRadius: pinSize / 2,
+                backgroundColor: 'black',
+                height: isSelected ? pinSize : 2,
+                marginBottom: isSelected ? pinSize / 2 : 0,
+              }}></View>
+          );
+        })}
+      </View>
+      <DialPad
+        onPress={item => {
+          if (item === 'del') {
+            setCode(prev => prev?.slice(0, prev?.length - 1));
+          } else if (typeof item === 'number') {
+            if (code?.length === pinLength) return;
+            setCode(prev => [...prev, item]);
+          }
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+// NOTE - This can be improved by using RNvector icons , Reanimated, Or Moti
