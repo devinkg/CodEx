d97c2028bd2a115ebdd4ebfa266bd16cca3a92d2
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-01-26 22:51:37 +0530

    new components applied

--- app/screens/LandingScreen.tsx
+++ app/screens/LandingScreen.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
 // Custom components
-import { DialPad } from '../components';
+import { DialPad, PinCodeFlatDot } from '../components';
 
 const LandingScreen = () => {
+  const [pincode, setPincode] = useState<number[]>([]);
+  const pinCodeLength = 4;
   return (
     <View style={styles.mainContainer}>
-      <DialPad />
+      <PinCodeFlatDot pinLength={4} pinCode={pincode} />
+      <DialPad
+        onPress={key => {
+          if (key === 'del') {
+            setPincode(prev => prev?.slice(0, prev?.length - 1));
+          } else if (typeof key === 'number') {
+            if (pincode?.length === pinCodeLength) return;
+            setPincode(prev => [...prev, key]);
+          }
+        }}
+      />
     </View>
   );
 };
